679ac7588fde613bb2ff39a37b56e28d5eafb986
felabel <felicityabel2016@gmail.com>
2024-01-13 01:22:44 +0100

    solved the quick sort algorithm

---
+++ sortingAlgorithms/quicksort.js
@@ -0,0 +1,23 @@
+function quickSort(arr) {
+    if(arr.length < 2){
+        return arr
+    }
+    let pivot = arr[arr.length - 1]
+    let left = []
+    let right = []
+
+    for (let i = 0; i < arr.length-1; i++){
+        // change this to < to sort in ascending order
+        if(arr[i] > pivot){
+            left.push(arr[i])
+        } else {
+            right.push(arr[i])
+        }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)]
+}
+
+const arr = [8,20,-2,-4,-6]
+console.log(quickSort(arr))
+// worst case -O(n^2) - for an already sorted array
+// avg case - O(nlogn)
