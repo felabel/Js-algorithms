5fdf7df97a7bea986007d3f5824a6ef6a6290416
felabel <felicityabel2016@gmail.com>
2024-01-13 02:14:12 +0100

    ADDED TIME COMPLEXITY for  the cartesian product algorithm

--- moreAlgos/cartesianProduct.js
+++ moreAlgos/cartesianProduct.js
@@ -11,3 +11,6 @@ function cartesianproduct(arr1, arr2){
 const arr1 = [1,3]
 const arr2 = [2,3,4]
 console.log(cartesianproduct(arr1, arr2))
+
+// Big-O = O(mn) - 2 arrays with different lenth
+// Big-O would be O(n^2) if both arrays had same length
